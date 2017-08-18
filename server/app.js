bc6ed9b46f8364d698f04d50b5904a35f1fa3e34
lishangxi <lishangxi@hualala.com>
2017-08-18 16:22:43 +0800

    remove ueditor

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import Controllers from './controllers'
 import {sessionConfig} from './config/session' ;
-import ueditor from "ueditor";
 const MongoStore = require('connect-mongo')(session);
 
 
@@ -58,27 +57,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-// app.use("/ueditor/ueditor", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
-//     // ueditor 客户发起上传图片请求
-//     if (req.query.action === 'uploadimage') {
-//         var foo = req.ueditor;
-//         var imgname = req.ueditor.filename;
-//         var img_url = '/images/ueditor/';
-//         //你只要输入要保存的地址 。保存操作交给ueditor来做
-//         res.ue_up(img_url);
-//     }
-//     //  客户端发起图片列表请求
-//     else if (req.query.action === 'listimage') {
-//         var dir_url = '/images/ueditor/';
-//         // 客户端会列出 dir_url 目录下的所有图片
-//         res.ue_list(dir_url);
-//     }
-//     // 客户端发起其它请求
-//     else {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.redirect('/ueditor/nodejs/config.json');
-//     }
-// }));
 
 
 Controllers(app)
